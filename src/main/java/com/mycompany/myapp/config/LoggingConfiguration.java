35e9c79aed89287ef4ee11333d6be0029aff57ac
Gaël MARZIOU <gael.marziou@actoll.com>
2016-08-23 13:37:22 +0200

    - Remove devtools from pom.xml - Import code from @ygokirmak LoggingConfiguration and LogstashConfigurator - Configure logbabck with debug and short scan period

--- src/main/java/com/mycompany/myapp/config/LoggingConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/LoggingConfiguration.java
@@ -1,73 +1,36 @@
 package com.mycompany.myapp.config;
 
-import ch.qos.logback.classic.AsyncAppender;
-import ch.qos.logback.classic.LoggerContext;
-import net.logstash.logback.appender.LogstashSocketAppender;
-import net.logstash.logback.stacktrace.ShortenedThrowableConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 @Configuration
-public class LoggingConfiguration {
+public class LoggingConfiguration implements ApplicationListener<ContextRefreshedEvent> {
 
     private final Logger log = LoggerFactory.getLogger(LoggingConfiguration.class);
 
-    private LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-
-    @Value("${spring.application.name}")
-    private String appName;
-
-    @Value("${server.port}")
-    private String serverPort;
-
-    @Value("${eureka.instance.instanceId}")
-    private String instanceId;
+    private JHipsterProperties properties;
+    private LogstashConfigurator logstashConfigurator;
 
     @Inject
-    private JHipsterProperties jHipsterProperties;
-
-    @PostConstruct
-    private void init() {
-        if (jHipsterProperties.getLogging().getLogstash().isEnabled()) {
-            addLogstashAppender();
-        }
+    public void setLogstashConfigurator(LogstashConfigurator logstashConfigurator) {
+        this.logstashConfigurator = logstashConfigurator;
     }
 
-    public void addLogstashAppender() {
-        log.info("Initializing Logstash logging");
+    @Inject
+    public void setProperties(JHipsterProperties properties) {
+        this.properties = properties;
+    }
 
-        LogstashSocketAppender logstashAppender = new LogstashSocketAppender();
-        logstashAppender.setName("LOGSTASH");
-        logstashAppender.setContext(context);
-        String customFields = "{\"app_name\":\"" + appName + "\",\"app_port\":\"" + serverPort + "\"," +
-            "\"instance_id\":\"" + instanceId + "\"}";
-
-        // Set the Logstash appender config from JHipster properties
-        logstashAppender.setSyslogHost(jHipsterProperties.getLogging().getLogstash().getHost());
-        logstashAppender.setPort(jHipsterProperties.getLogging().getLogstash().getPort());
-        logstashAppender.setCustomFields(customFields);
-
-        // Limit the maximum length of the forwarded stacktrace so that it won't exceed the 8KB UDP limit of logstash
-        ShortenedThrowableConverter throwableConverter = new ShortenedThrowableConverter();
-        throwableConverter.setMaxLength(7500);
-        throwableConverter.setRootCauseFirst(true);
-        logstashAppender.setThrowableConverter(throwableConverter);
-
-        logstashAppender.start();
-
-        // Wrap the appender in an Async appender for performance
-        AsyncAppender asyncLogstashAppender = new AsyncAppender();
-        asyncLogstashAppender.setContext(context);
-        asyncLogstashAppender.setName("ASYNC_LOGSTASH");
-        asyncLogstashAppender.setQueueSize(jHipsterProperties.getLogging().getLogstash().getQueueSize());
-        asyncLogstashAppender.addAppender(logstashAppender);
-        asyncLogstashAppender.start();
-
-        context.getLogger("ROOT").addAppender(asyncLogstashAppender);
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (properties.getLogging().getLogstash().isEnabled()) {
+            logstashConfigurator.addLogstashAppender();
+            log.info("Added Logstash Appender");
+        }
     }
 }
